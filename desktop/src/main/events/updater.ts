ef95706bbc3a71833d6b9e0b355775443d9e992e
Triss <myb619@gmail.com>
2026-04-24 17:02:08 +0900

    fix: suppress unused param lint error in updater stub

--- desktop/src/main/events/updater.ts
+++ desktop/src/main/events/updater.ts
@@ -1,5 +1,6 @@
 import { BrowserWindow } from 'electron'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function registerUpdater(_win: BrowserWindow): void {
   console.log('Update functionality disabled for X86 fork')
 }
